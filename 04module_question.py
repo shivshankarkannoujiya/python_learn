e1a80195ed3e9f1260e63368d3399db5b78f06f9
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2026-04-03 14:18:33 +0530

    Add random selection functionality for students and subjects

---
+++ 04module_question.py
@@ -0,0 +1,12 @@
+import random
+
+students = ["Rahul", "Priya", "Amit", "Sneha", "Rohan"]
+
+#  Lucky Winner: Sneha
+winner = random.choice(students)
+print("🏆 Lucky Winner:", winner)
+
+
+subjects = ["Maths", "Physics", "Chemistry", "Python", "English"]
+subject = random.choice(subjects)
+print("Random sub: ", subject)
