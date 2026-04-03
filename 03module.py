47cce274e8732661b386285a447bc6ec3944faac
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2026-04-03 14:14:09 +0530

    Add math module with examples for trigonometric functions and logarithm

---
+++ 03module.py
@@ -0,0 +1,28 @@
+# 3. MATH MODULE
+
+import math
+
+# print(math.pi)
+
+
+# NOTE: math.log10(x) → Base-10 Logarithm
+
+
+# print(math.log10(100))
+
+
+# NOTE: sin(), cos(), tan()
+# Degrees → Radians formula: radians = degrees × (π/180)
+
+angle = 90
+
+rad = math.radians(angle)
+
+
+# print(math.sin(rad))
+# print(math.cos(rad))
+# print(math.tan(rad))
+
+
+
+# help(math.cos)
