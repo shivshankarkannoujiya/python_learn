46f27ff7d9022443a35a54f0dc018553adc09325
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2026-04-03 14:06:57 +0530

    Add random module with examples of random number generation

---
+++ 02module.py
@@ -0,0 +1,20 @@
+# 2. RANDOM MODULE
+
+# NOTE: random() => 0 to 1 (float number)
+
+import random
+
+x = random.random()
+# print(x)
+
+
+# NOTE: randint(a, b)
+num = random.randint(1, 10)
+# print(num)
+
+
+# NOTE: randrange(start, stop, step) → Range se random number
+
+
+num = random.randrange(0, 11, 2)
+# print(num)
