2be3b37a3b131ce4a9ba717dd1068689400caa8c
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2026-06-15 05:28:03 +0530

    Add exception handling example in one.py

---
+++ 02Exception_handling/one.py
@@ -0,0 +1,15 @@
+# num = 10
+# answer = num / 0
+
+# print(answer)
+# print("Program Finished")
+
+
+try:
+    num = 10
+    answer = num / 0
+    print(answer)
+except:
+    print("Something went wrong.")
+
+print("Program Finished")
